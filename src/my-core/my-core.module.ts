7ee9bde4deaae2206b05561ef6fa609fa01a4c6c
Javier Martin <jmartin@grupoloyal.com>
2018-09-07 00:48:49 +0200

    Validadores

--- src/my-core/my-core.module.ts
+++ src/my-core/my-core.module.ts
@@ -3,13 +3,14 @@ import { CommonModule } from '@angular/common';
 import { PIPES_CADENAS } from './pipes/cadenas.pipe';
 import { SizerComponent } from './components/sizer.component';
 import { PIPES_NUMERICOS } from './pipes/numericos.pipe';
+import { MIS_VALIDADORES } from './directives/validadores.directive';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [PIPES_CADENAS, SizerComponent, PIPES_NUMERICOS],
-  exports: [PIPES_CADENAS, SizerComponent, PIPES_NUMERICOS]
+  declarations: [PIPES_CADENAS, SizerComponent, PIPES_NUMERICOS, MIS_VALIDADORES],
+  exports: [PIPES_CADENAS, SizerComponent, PIPES_NUMERICOS, MIS_VALIDADORES]
 })
 export class MyCoreModule {
   constructor( @Optional() @SkipSelf() parentModule: MyCoreModule) {
